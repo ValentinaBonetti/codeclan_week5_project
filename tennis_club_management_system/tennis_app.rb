192ea8d87d4f8129e984184803a50319eb36d81a
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-01 15:03:44 +0100

    Build basic website layout and index page

---
+++ tennis_club_management_system/tennis_app.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+# require_relative('controllers/manager_controller')
+# require_relative('controllers/coach_controller')
+# require_relative('controllers/member_controller')
+
+get '/' do
+  erb( :index )
+end
